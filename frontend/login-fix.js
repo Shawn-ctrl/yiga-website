3b3929a1ae954314ea609c8849f1508b718be344
Brown Shawn <brownshawn4494@gmail.com>
2025-11-27 15:26:39 +0300

    FIX: Remove broken showLoginModal handler

---
+++ frontend/login-fix.js
@@ -0,0 +1,16 @@
+﻿// FIX: Add missing showLoginModal function
+function showLoginModal() {
+    console.log("Login modal triggered");
+    alert("Admin login will be available soon");
+}
+
+// Remove the broken handler after page loads
+setTimeout(function() {
+    const adminLinks = document.getElementsByTagName('a');
+    for (let link of adminLinks) {
+        if (link.onclick && link.onclick.toString().includes('showLoginModal')) {
+            link.onclick = null;
+            link.href = "#";
+        }
+    }
+}, 100);
