2d832502776c11105e1c3501562ca41497a68ab9
Shawn Ndombi <shawn@example.com>
2026-03-15 23:50:30 +0300

    Revert "Add back missing fetchApplications function"
    This reverts commit 8456aeeee71018eea3c227f84bdcc83f5dea0f78.

--- src/App.js
+++ src/App.js
@@ -399,20 +399,6 @@ function App() {
 
 
 
-
-  const fetchApplications = async () => {
-    try {
-      const { data, error } = await supabase
-        .from('application')
-        .select('*')
-        .order('created_at', { ascending: false });
-
-      if (error) throw error;
-      setApplications(data || []);
-    } catch (error) {
-      console.error('Error fetching applications:', error);
-    }
-  };
   const handleLogout = () => {
     setAuthToken('');
     setIsLoggedIn(false);
